f1e3cea8a06702f501ed0e17aaa27584fbab6c80
jayrajthakor <jayrajthakor25@gmail.com>
2022-05-21 17:17:15 -0400

    adding header for todo list

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {useState} from "react";
+import Header from "./components/header";
 
 export default function App() {
+    const [todos, setTodos] = useState([
+        { text: 'Learn React', key: '1' },
+        { text: 'Learn Redux', key: '2' },
+        { text: 'Learn React Hooks', key: '3' },
+        { text: 'Learn React Native', key: '4' },
+    ]);
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
+        <Header/>
+        <View style={styles.content}>
+            <View style={styles.list}>
+                {
+                    todos.map(item =>
+                        <Text key={item.key}>{item.text}</Text>
+                    )
+                }
+            </View>
+        </View>
     </View>
   );
 }
@@ -15,7 +31,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+    content: {
+        padding: 40,
+    },
+    list: {
+        marginTop: 20,
+    }
 });
